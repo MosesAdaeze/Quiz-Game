1ed6061afca328d3ee28a23c9a5ab329b8f8d6e1
Kloss <108338208+MosesAdaeze@users.noreply.github.com>
2023-01-15 15:50:16 +0100

    adjust

--- style.js
+++ style.js
@@ -1,39 +1,39 @@
 const startButton = document.getElementById('start-btn')
-const quizContainerElement = document.getElementById('quiz-container')
-const questionElement = document.getElementById('question')
+const quizContainerElement = document.getElementById('container')
+// const questionElement = document.getElementById('question')
 
 
-let shuffledQuestions, currentQuestionIndex
+// let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
 
 function startGame() {
     console.log('Started')
     startButton.classList.add('hide')
-    shuffledQuestions = questions.sort(() => Math.random() - .5)
-    currentQuestionIndex = 0
+    // shuffledQuestions = questions.sort(() => Math.random() - .5)
+    // currentQuestionIndex = 0
     quizContainerElement.classList.remove('hide')
-    setNextQuestion()
+    // setNextQuestion()
 }
 
-function setNextQuestion() {
-    showQuestion(shuffledQuestions[currentQuestionIndex])
-}
+// function setNextQuestion() {
+//     showQuestion(shuffledQuestions[currentQuestionIndex])
+// }
 
-function showQuestion(question) {
-    questionElement.innerText = question.question
-}
+// function showQuestion(question) {
+//     questionElement.innerText = question.question
+// }
 
-function selectAnswer() {
+// function selectAnswer() {
 
-}
+// }
 
-const question = [
-    {
-      question: 'What is 2 + 2?',
-      answers: [
-        { text: '4', correct: true },
-        { text: '22', correct: false }
-      ]
-    }
-]
+// const question = [
+//     {
+//       question: 'What is 2 + 2?',
+//       answers: [
+//         { text: '4', correct: true },
+//         { text: '22', correct: false }
+//       ]
+//     }
+// ]
