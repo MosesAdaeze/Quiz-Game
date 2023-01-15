d61e46ac2e40eb6d49ad24a74e681a7d39d58cd0
Kloss <108338208+MosesAdaeze@users.noreply.github.com>
2023-01-15 15:21:41 +0100

    game

--- style.js
+++ style.js
@@ -1,10 +1,39 @@
-// const startButton = document.getElementById('start-btn')
-// const levelContainerElement = document.getElementById('level-container')
+const startButton = document.getElementById('start-btn')
+const quizContainerElement = document.getElementById('quiz-container')
+const questionElement = document.getElementById('question')
 
-// startButton.addEventListener('click', startGame)
 
-// function startGame() {
-//     console.log('Started')
-//     startButton.classList.add('hide')
-//     questionContainerElement.classList.remove('hide')
-// }
+let shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener('click', startGame)
+
+function startGame() {
+    console.log('Started')
+    startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    quizContainerElement.classList.remove('hide')
+    setNextQuestion()
+}
+
+function setNextQuestion() {
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+    questionElement.innerText = question.question
+}
+
+function selectAnswer() {
+
+}
+
+const question = [
+    {
+      question: 'What is 2 + 2?',
+      answers: [
+        { text: '4', correct: true },
+        { text: '22', correct: false }
+      ]
+    }
+]
