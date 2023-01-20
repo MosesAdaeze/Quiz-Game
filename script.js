ba4de891bfa75ceaeb1cbe547720b2ebe23fbab1
Kloss <108338208+MosesAdaeze@users.noreply.github.com>
2023-01-20 22:32:52 +0100

    Commited

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-function openOptionModal() {
-  document.getElementById('option-modal').style.display = "flex"
+function openOptionlevel() {
+  document.getElementById('option-level').style.display = "flex"
 }
 
+function openOptions() {
+  document.getElementById('options').style.display = "flex"
+}
+
+function closeOptionlevel(){
+  document.getElementById('option-level').style.display = "none"
+}
+
+function closeOptions(){
+  document.getElementById('options').style.display = "none"
+}
+
+function openrule() {
+  document.getElementById('rules').style.display=  "flex"
+  document.getElementById('options').style.display= "none"
+}
+
+function closerule() {
+  document.getElementById('rules').style.display= "none"
+}
